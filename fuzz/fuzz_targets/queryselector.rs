522ac4c1589fe728c7b908ecc60f49fd6fd737f3
Evan Richter <evanjrichter@gmail.com>
2022-09-05 13:43:08 -0500

    update fuzz harnesses

--- fuzz/fuzz_targets/queryselector.rs
+++ fuzz/fuzz_targets/queryselector.rs
@@ -11,7 +11,7 @@ const HTML: &str = r#"
 "#;
 
 fuzz_target!(|data: &str| {
-    let dom = tl::parse(HTML, tl::ParserOptions::default());
+    let dom = tl::parse(HTML, tl::ParserOptions::default()).unwrap();
     let iter = dom.query_selector(data);
     for _ in iter {
         // do nothing
